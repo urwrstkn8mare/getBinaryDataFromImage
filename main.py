ca55084e0cea1dd10166ea75063f36556a59f2d7
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2020-02-11 18:53:40 +0800

    Just some stuff

--- main.py
+++ main.py
@@ -1,32 +1,62 @@
-def toBin(integer):
+import time
+
+
+def progressBar(percentage: float, endr=True):
+    toPrint = ""
+    outOfTen = int(percentage.__round__(-1) / 10)
+    i = 1
+    while i <= outOfTen:
+        toPrint += "\u25A0"
+        i += 1
+    i = 1
+    while i <= 10 - outOfTen:
+        toPrint += "\u25A1"
+        i += 1
+    toPrint += " " + str(percentage.__round__(1)) + "%"
+    if endr:
+        print(toPrint, end="\r")
+    else:
+        print(toPrint)
+
+
+def toBin(integer: int) -> str:
     return f'{integer:08b}'
 
 
-def main(filename):
+def main(filename: str):
+    if filename.find("\\") != -1 or filename.find("/") != -1:
+        print("Only input the name of the file, not the path.\n" +
+              "It should be in \"test/in\". ")
+        return
     from PIL import Image
-    im = Image.open("in/" + filename).convert(
-        mode="RGB")
-    im.thumbnail((255, 255))
-    height, width = im.size
-    print(str(height) + ", " + str(width))
+    im = Image.open("test/in/" + filename).convert(
+        mode="RGB").rotate(90)
+    im.thumbnail((16, 16))
+    width, height = im.size
     pix = im.load()
     binary = toBin(width) + toBin(height) + toBin(24)
 
     totalPixels = height * width
 
     i = 0
-    while i < height:
+    while i < width:
         ii = 0
-        while ii < width:
+        while ii < height:
             red, green, blue = pix[i, ii]
             binary += toBin(red) + toBin(green) + toBin(blue)
-            print(str((((i*height)+ii+1) / totalPixels) * 100) + "%")
+            endr = True
+            if i == width - 1 and ii == height - 1:
+                endr = False
+            progressBar((((i*height)+ii+1) / totalPixels) * 100, endr)
             ii += 1
         i += 1
 
-    f = open("out/binary_" + filename + "_.txt", "w+")
+    f = open("test/out/binary_" + filename + "_.txt", "w+")
     f.write(binary)
     f.close()
+    print("Total bits: " + str(binary.__len__()))
+    return
 
 
-main('apple-splash.jpg')
+# main(input("File path: "))
+main("unnamed.gif")
